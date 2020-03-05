63e1e326ed76fbf68f6cc58e7e13607e19815b35
Shuo Wang <45891992+Kev1nWangsus@users.noreply.github.com>
2020-03-04 20:41:55 -0800

    Update gold.py

--- gold.py
+++ gold.py
@@ -1,3 +1,11 @@
+# Author : Shuo Wang (Kevin)
+
+# This program helps detect any available space for your target course
+#  iff waitlist is not enabled and you want to change schedule due to 
+#  time conflict or other personal reasons
+
+import smtplib
+import argparse
 import requests
 import json
 import sqlite3
@@ -9,59 +17,65 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.utils import parseaddr, formataddr 
 
-import smtplib
-import argparse
 
 def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
-from_addr = "1875689740ws@gmail.com"
-password = "kmcechrwxrgqlcan"
-to_addr = "shuo_wang@ucsb.edu"
-smtp_server = "smtp.gmail.com"
-
-msg = MIMEMultipart()
-msg['From'] = _format_addr("GOLD %s" % from_addr)
-msg['To'] = _format_addr("Receiver %s" % to_addr)
-msg['Subject'] = Header("GOLD Enrollment", 'utf-8').encode()
-
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     return text
 
-course = {'q':'20202', 'id':'PHYS2'}
-enrollcode = "38513"
+# Be sure to replace below with your email address and security password
+sender = "" 
+password = ""
 
-while(not time.sleep(5)):
+# add target recipients here
+recipients = []
+smtp_server = "smtp.gmail.com"
+
+course = {'q':'20202', 'id':'PHYS2'} 
+# replace with your preferred quarter and course title
+enrollcode = "38513"
+# replace with your preferred course enrollcode
+
+timeCount = 0
+
+while(not time.sleep(60)):
+    msg = MIMEMultipart()
+    msg['Subject'] = "New Update!"
+    msg['From'] = "GOLD Crasher"
+    msg['To'] = ", ".join(recipients)    
+
     response = requests.get('https://web.gogaucho.app/api/sche/getClassByID', params = course)
     print("Updated!")
 
     jdata = json.loads(jprint(response.json()))
 
+    server = smtplib.SMTP(host=smtp_server, port=587)
+    server.starttls()
+    server.login(sender, password)
+
     count = 0
     msg.attach(MIMEText("Course Title: %s \n" % course['id'], 'plain'))
     for i in jdata["classSections"]:
         space = i["maxEnroll"] - i["enrolledTotal"]
-        if space and i["enrollCode"] == enrollcode:
-            print("The course you are looking for has %d spots available." % (space))
-            msg.attach(MIMEText("! The course with enrollment code %s you are looking has %d available space !" % (enrollcode, space), 'plain'))
-            server = smtplib.SMTP(host=smtp_server, port=587)
-            server.starttls()
-            server.login(from_addr, password)
-            server.sendmail(from_addr, [to_addr], msg.as_string())
+        if space:
+            count += 1
+            if i["enrollCode"] == enrollcode:
+                if not timeCount:
+                    print("Found!")
+                    msg.attach(MIMEText("The course with enrollment code %s has %d available space right now!" % (enrollcode, space), 'plain'))
+                    server.sendmail(sender, recipients, msg.as_string())
+                    server.quit()
+                    timeCount = 10
+                else:
+                    timeCount -= 1
+    if not count:
+        if not timeCount:
+            msg.attach(MIMEText("All sections are full! \n Log in to see if waitlist is enabled. \n", 'plain'))
+            server.sendmail(sender, recipients, msg.as_string())
             server.quit()
-    #     elif space > 0:
-    #         print("Course with enrollcode %s has %d spot(s) available." % (i["enrollCode"], space))
-    #         msg.attach(MIMEText("Course with enrollcode %s has %d spot(s) available.\n" % (i["enrollCode"], space), 'plain'))
-    #         count += 1
-    # if count == 0:
-    #     print("Sorry, no section or course is availble right now :(")
-    #     msg.attach(MIMEText("Sorry, no section or course is availble right now :(", 'plain'))
-
-
-# if __name__ == '__main__':
-#     main()
-
-
-
+            timeCount = 10
+        else:
+            timeCount -= 1
